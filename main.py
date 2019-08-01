231f83859ba9b4d833787496a409d52bf84d35ac
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-08-01 17:21:18 +0200

    Optimized conditions

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ def main():
     year = int(year)
 
     # Testing if the 'year' is a leap year
-    if year % 400 == 0:
-        leap = True
-    elif year % 100 == 0:
-        leap = False
-    elif year % 4 == 0:
+    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
         leap = True
     else:
         leap = False
