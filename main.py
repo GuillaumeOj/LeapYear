62941a8610973b180c173558a6408146f339773e
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-08-02 11:20:15 +0200

    Add 'try' block to verify user's input

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ def main():
         Ce programme teste une année donnée par un utilisateur
         et indique si celle-ci est bissextile ou non
     """
-    
-    # Asking user for a year
-    year = input("Give me a year: ")
-    year = int(year)
+    year_confirmation = False
+
+    while year_confirmation == False:
+        try:
+            # Asking user for a year
+            year = input("Give me a year: ")
+            year = int(year)
+        except:
+            print("Please give me a correct year !")
+            continue
+        
+        year_confirmation = True
 
     # Testing if the 'year' is a leap year
     if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
